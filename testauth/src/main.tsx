277affdeadaf03397662edb171f45e0c90b3e843
sanjanaravikumar-az <sanjana.ravikumar.az@gmail.com>
2026-04-07 15:46:39 -0400

    chore: mid work

--- testauth/src/main.tsx
+++ testauth/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
+import amplifyconfig from '../amplify_outputs.json';
 Amplify.configure(amplifyconfig);
 
 createRoot(document.getElementById('root')!).render(
